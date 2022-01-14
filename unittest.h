b2f961560da337147cfa51babd2848d5fc0a257a
Pratik Rawal <ge26pex@mytum.de>
2022-01-14 12:45:05 +0100

    Update unittest.h removed floor()

--- unittest.h
+++ unittest.h
@@ -2,37 +2,38 @@
 #include<iostream>
 #include<cmath>
 #include"functions.h"
+#include"functions.cpp"
 const double pi = 3.1415926535;
 double sol[10] = {
     1,
-    sin(pi),
-    cos(pi),
-    tan(pi),
+    sin(pi/2),
+    cos(0),
+    tan(0),
     1/tan(0),
     1/cos(pi),
     1,
     log(pi),
     log10(pi),
-    exp(pi)
+    1
 };
 double testx[10] = {
     1,
+    pi/2,
+    0,
+    0,
+    0,
     pi,
+    pi/2,
     pi,
     pi,
     0,
-    pi,
-    1,
-    pi,
-    pi,
-    pi,
 };
 
 int unittest()
 {
     for (auto i = 0; i < 10;i++)
     {
-        if (floor((eval(i, testx[i])))!=floor(sol[i]))
+        if ((eval(i, testx[i]))!=sol[i])
         {
             return 0; 
         }
