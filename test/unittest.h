c6e9fc399953217a1c8c6805ae27c4a7f2b22076
Yashas Gandhi <y.gandhi@tum.de>
2022-01-14 17:54:06 +0100

    Update unittest.h

--- test/unittest.h
+++ test/unittest.h
@@ -1,44 +1,51 @@
-#include<cmath>
-#include<iostream>
-#include<cmath>
-#include"include/functions.h"
+#include "include/functions.h"
+#include <cmath>
+#include <iostream>
 
-const double pi = 3.1415926535;
+constexpr double pi = 3.1415926535;
 
-double sol[10] = {
-    1,
-    sin(pi/2),
-    cos(0),
-    tan(0),
-    1/tan(0),
-    1/cos(pi),
-    1,
-    log(pi),
-    log10(pi),
-    1
-};
+constexpr bool evalTest() {
+  double sol[10] = {
+      1, sin(pi / 2.), cos(0.),   tan(0.), 1. / tan(pi / 2.), 1 / cos(pi),
+      1, log(pi),      log10(pi), 1};
 
-double testx[10] = {
-    1,
-    pi/2,
-    0,
-    0,
-    0,
-    pi,
-    pi/2,
-    pi,
-    pi,
-    0,
-};
-
-int unittest()
-{
-    for (auto i = 0; i < 10;i++)
-    {
-        if ((eval(i, testx[i]))!=sol[i])
-        {
-            return 0; 
-        }
+  double testx[10] = {
+      1., pi / 2, 0., 0., pi / 2., pi, pi / 2., pi, pi, 0.,
+  };
+  for (auto i = 0; i < 10; i++) {
+    if ((eval(i, testx[i])) != sol[i]) {
+      std::cout << "Check eval() for function number"<<i<<"\n";
+      return 1;
     }
+  }
+  return 0;
+}
+
+constexpr bool evalDiffTest() {
+  double solDiff[10] = {0.,
+                        cos(pi),
+                        -sin(pi / 2.),
+                        1. / (cos(pi) * cos(pi)),
+                        -1. / (sin(pi / 2.) * sin(pi / 2.)),
+                        tan(0.) / cos(0.),
+                        -1. / (sin(pi / 2.) * tan(pi / 2.)),
+                        1.,
+                        0.,
+                        exp(0.)};
+  double test[10] = {
+      1, pi, pi / 2, pi, pi / 2, 0, pi/2., 1., INFINITY, 0.,
+  };
+  for (auto i = 0; i < 10; i++) {
+    if (evalDiff(i, test[i]) != solDiff[i]) {
+      std::cout << "Check evalDiff() for function number"<<i<<"\n";
+      return 1;
+    }
+  }
+  return 0;
+}
+
+constexpr bool unitTest() {
+  if (evalTest() == 1 || evalDiffTest() == 1)
     return 1;
+  return 0;
 }
