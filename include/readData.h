862372a6510cead495a5e2b3908d80e2460b7433
Pratik Rawal <ge26pex@mytum.de>
2022-02-02 22:56:34 +0100

    Added comments and formatted.

--- include/readData.h
+++ include/readData.h
@@ -1,6 +1,10 @@
 #ifndef READ_DATA_H
 #define READ_DATA_H
 
+/**
+Here we define the function that is responsible to read the values from json data file and assign the value 
+to respective variables
+*/
 #include "equation.h"
 #include "json.hpp"
 
