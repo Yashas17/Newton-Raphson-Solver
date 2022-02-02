70c8681d95cbe5bde706f3954651a44c9ad21316
Pratik Rawal <ge26pex@mytum.de>
2022-02-02 22:00:41 +0100

    Added comments and formatted

--- include/functions.h
+++ include/functions.h
@@ -1,11 +1,14 @@
 #ifndef FUNCTIONS_H
 #define FUNCTIONS_H
+/**These are the functions responsible for returning the value of the actual
+**function and the differentiated function at any particular value of x
+**/
 
 double eval(const unsigned int f,
-            const double x); // Function returning the value of special function
-                             // at given point
+            const double x);                // Function returning the value of special function
+                                            // at given point
 double evalDiff(const unsigned int f,
-                const double x); // Function returning the value of derivative
-                                 // of special function at given point
+                const double x);            // Function returning the value of derivative
+                                            // of special function at given point
 
 #endif
