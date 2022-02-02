80de64b35a0316776e202ded8496066e8a76ea50
Yashas Tejaskumar Gandhi <y.gandhi@tum.de>
2022-02-02 22:33:36 +0100

    Optimizations

--- src/main.cpp
+++ src/main.cpp
@@ -12,17 +12,5 @@ int main() {
   int miter;                    // Maximum iterations
   equation eq;                  // Vector to store equation
   readData(x0, tol, miter, eq); // Read Data
-
-  std::ofstream logfile;
-  logfile.open("log.csv", std::ios_base::app);
-  for (auto i = 0; i < 10; i++) {
-
-    auto start = std::chrono::system_clock::now();
-    eq.solve(x0, tol, miter); // Solve the equation
-    auto end = std::chrono::system_clock::now();
-    auto elapsed =
-        std::chrono::duration_cast<std::chrono::microseconds>(end - start);
-    logfile << elapsed.count() << ',';
-  }
-  logfile.close();
+  eq.solve(x0, tol, miter);     // Solve the equation
 }
